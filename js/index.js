6e19c437b67567b5f76afdf67f2eaa65977000a4
Zukhriddin <example@gmail.com>
2023-08-14 11:48:51 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+"use strict";
+//#8
+//alert("Hello JavaScript")
+
+//const place = confirm("Are you here ?");
+//console.log(place);
+
+//const currentPlace = prompt("Where are you ?", "Zukhriddin");
+//console.log(typeof currentPlace);
+
+//const age = +prompt("How old are you ?", "18");
+//console.log(age + 10);
+
+// const favoriteColor = [];
+
+// favoriteColor[0] = prompt("What's your favourite color #1", "");
+// favoriteColor[1] = prompt("What's your favourite color #2", "");
+// favoriteColor[2] = prompt("What's your favourite color #3", "");
+
+// console.log(typeof favoriteColor);
+
+//#9
+
+// const chanel = prompt("What's your favourite youtuber ?");
+// console.log(`https://youtube.com/${chanel}`);
+
+//const user = "Samar";
+//const job = "JS develloper";
+
+//console.log(`User name is ${user}, he is ${job}`);
+
+//#10 Operatorlar
+
+//console.log(8 + +" 9");
+
+// let inc = 10;
+// let decr = 10;
+
+// console.log(inc++);
+// console.log(decr--);
+
+// console.log(inc);
+// console.log(decr);
+
+// console.log(10 % 3);
+// console.log(5 * 5 == 25);
+// console.log(5 * 5 === 25);
+
+// && = va || = yoki
+
+// const isAge = true;
+// const isClose = false;
+// const isChecked = false
+// console.log(isAge && !isClose && !isChecked);
